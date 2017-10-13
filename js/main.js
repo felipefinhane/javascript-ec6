9db5baf7c8ff30c72cff4d018bad2d0a05dbfc5b
Felipe Finhane de Paula <felipefinhane@gmail.com>
2017-10-13 10:17:17 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//var bob = {
+//    _name:"Bob",
+//    _friends: ["Guilheme","Camila","Murilo"],
+//    printFriends: function printFriends(){
+//        var self = this;
+//        this._friends.forEach(function(f) {
+//            return console.log(self._name  + " knows " + f);
+//        });
+//    }
+//}
+//
+//bob.printFriends();
+
+var color = function color() {
+    return console.log("red");
+};
+color();
+
+var bob = {
+    _name: "Bob",
+    _friends: ["Guilheme", "Camila", "Murilo"],
+    printFriends: function printFriends() {
+        var _this = this;
+
+        this._friends.forEach(function (f) {
+            return console.log(_this._name + " knows " + f);
+        });
+    }
+};
+
+bob.printFriends();
---
+++ src/app.js
@@ -0,0 +1,28 @@
+//var bob = {
+//    _name:"Bob",
+//    _friends: ["Guilheme","Camila","Murilo"],
+//    printFriends: function printFriends(){
+//        var self = this;
+//        this._friends.forEach(function(f) {
+//            return console.log(self._name  + " knows " + f);
+//        });
+//    }
+//}
+//
+//bob.printFriends();
+
+var color = () => console.log("red");
+color();
+
+
+
+var bob = {
+    _name:"Bob",
+    _friends: ["Guilheme","Camila","Murilo"],
+    printFriends: function printFriends(){
+        this._friends.forEach(f => console.log(this._name + " knows " + f));
+    }
+}
+
+bob.printFriends();
+
